bca51d9747dbd49afdf5d52c5abc6a30588bf8e3
shadrqen <shadrqen@outlook.com>
2022-02-22 13:47:17 +0300

    refactor: reorganize default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
+    <header>
+      Header
+    </header>
     <v-main>
       <router-view />
     </v-main>
+    <footer>
+      Footer
+    </footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
-    return {}
-  }
+  name: 'DefaultLayout'
 }
 </script>
