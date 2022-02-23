11da5393aa2aef2dd0c5b05a2b3f22d446f32196
shadrqen <shadrqen@outlook.com>
2022-02-23 18:30:45 +0300

    bug: fix set-login-dialog mutation misname

--- store/user/mutations.ts
+++ store/user/mutations.ts
@@ -4,7 +4,7 @@ import { AuthState } from '~/types/vuex'
 export enum AuthMutations {
   /* eslint-disable no-unused-vars */
   SET_USER_LOGGED_IN = 'SET_USER_LOGGED_IN',
-  SET_USER_IS_LOGGED_IN = 'SET_USER_IS_LOGGED_IN',
+  SET_LOGIN_DIALOG = 'SET_LOGIN_DIALOG',
   SET_REGISTRATION_DIALOG = 'SET_REGISTRATION_DIALOG',
   SET_ACCESS_TOKEN = 'SET_ACCESS_TOKEN'
   /* eslint-enable no-unused-vars */
@@ -14,7 +14,7 @@ export const mutations: MutationTree<AuthState> = {
   [AuthMutations.SET_USER_LOGGED_IN]: (state: AuthState, payload: boolean) => {
     state.userLoggedIn = payload
   },
-  [AuthMutations.SET_USER_IS_LOGGED_IN]: (state: AuthState, payload: boolean) => {
+  [AuthMutations.SET_LOGIN_DIALOG]: (state: AuthState, payload: boolean) => {
     state.loginDialog = payload
   },
   [AuthMutations.SET_REGISTRATION_DIALOG]: (state: AuthState, payload: boolean) => {
