d3c1b7f9132da72c93a00f3ee04395f55dd8b048
shadrqen <shadrqen@outlook.com>
2022-02-24 15:38:07 +0300

    feat: add user setting dialog

---
+++ components/LatestOrder.vue
@@ -0,0 +1,75 @@
+<template>
+  <base-data-table
+    :items="latestOrders"
+    :items-per-page="itemsPerPage"
+    :headers="headers"
+    card-class="mt-10"
+  >
+    <template #table-sub-title>
+      Latest orders
+    </template>
+  </base-data-table>
+</template>
+
+<script>
+import BaseDataTable from '@/components/Base/BaseDataTable'
+export default {
+  name: 'LatestOrder',
+  components: {
+    BaseDataTable
+  },
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Order UUID',
+          align: 'start',
+          sortable: false,
+          value: 'uuid'
+        },
+        {
+          text: 'Status',
+          value: 'status'
+        },
+        {
+          text: 'Download Invoice',
+          align: 'center',
+          value: 'invoice'
+        }
+      ],
+      latestOrders: [
+        {
+          uuid: '3298432984434-2345s0-sdfsdfsfsdfs',
+          status: 'Open',
+          chipColor: 'primary'
+        },
+        {
+          uuid: '209348230949-23324-3j423-sdfssdfs',
+          status: 'Pending payment',
+          chipColor: 'orange'
+        },
+        {
+          uuid: '3298432984434-2345s0-sdfsdffsdfsd',
+          status: 'Paid',
+          chipColor: 'green lighten-1'
+        },
+        {
+          uuid: '083239823432-43500-23hd73-sdsdfjs',
+          status: 'Shipped',
+          chipColor: 'green darken-3'
+        },
+        {
+          uuid: '083453454562-34553-34s723-ssdces',
+          status: 'Cancelled',
+          chipColor: ''
+        }
+      ],
+      itemsPerPage: 5
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/UserDetail.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-row
+    no-gutters
+  >
+    <v-col
+      cols="12"
+      sm="3"
+    >
+      <v-avatar
+        size="100"
+      >
+        <img
+          :src="require('@/assets/profile.jpg')"
+          alt="Profile"
+        >
+      </v-avatar>
+    </v-col>
+    <v-col>
+      <v-divider vertical />
+    </v-col>
+    <v-col
+      cols="12"
+      sm="9"
+    >
+      <v-row no-gutters>
+        <v-col
+          v-for="(detail, index) in userDetails"
+          :key="index"
+          cols="4"
+        >
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-subtitle v-text="detail.key" />
+                <v-list-item-content
+                  v-text="detail.value"
+                />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'UserDetails',
+  data () {
+    return {
+      userDetails: [
+        {
+          key: 'Name',
+          value: 'John Rollingston'
+        },
+        {
+          key: 'Phone number',
+          value: '(559) 979-6096'
+        },
+        {
+          key: 'Address',
+          value: '1285 Fallen Pioneer Heights, Dallas, TX'
+        },
+        {
+          key: 'Date joined',
+          value: '14. 9. 2020.'
+        },
+        {
+          key: 'Email',
+          value: 'j.sharp@hotmail.com'
+        },
+        {
+          key: 'Marketing preferences',
+          value: 'No'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
