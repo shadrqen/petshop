ea752cd8737bfb9bb0116cf591d128c4b4dd9edf
shadrqen <shadrqen@outlook.com>
2022-02-25 17:53:28 +0300

    feat: add vuex persistedstate

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vuex-persisted-state', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
